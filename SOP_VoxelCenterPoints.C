ccc7c03e194d8781cfcd110034645d0cf7b7629c
Mykola Konyk <mykola@konyk.org>
2016-03-25 01:57:52 -0400

    Work in progress.

--- SOP_VoxelCenterPoints.C
+++ SOP_VoxelCenterPoints.C
@@ -8,6 +8,8 @@
 #include <GEO/GEO_Detail.h>
 #include <GEO/GEO_PrimPoly.h>
 #include <GU/GU_Detail.h>
+#include <GU/GU_PrimVolume.h>
+
 
 PRM_Template
 SOP_VoxelCenterPoints::myTemplateList[] = {
@@ -23,7 +25,8 @@ SOP_VoxelCenterPoints::myConstructor(OP_Network* network, const char* name, OP_O
 
 
 SOP_VoxelCenterPoints::SOP_VoxelCenterPoints(OP_Network* network, const char* name, OP_Operator* op) :
-    SOP_Node(network, name, op)
+    SOP_Node(network, name, op),
+    m_group(nullptr)
 {
 
 }
@@ -38,20 +41,51 @@ SOP_VoxelCenterPoints::~SOP_VoxelCenterPoints()
 OP_ERROR
 SOP_VoxelCenterPoints::cookMySop(OP_Context& context)
 {
-    if(error() >= UT_ERROR_ABORT)
+    if(lockInputs(context) >= UT_ERROR_ABORT)
     {
         return error();
     }
 
-    gdp->clearAndDestroy();
+    duplicateSource(0, context);
 
-    UT_AutoInterrupt boss("Creating Voxel Center Points");
-    if(boss.wasInterrupted())
+    if(cookInputGroups(context) >= UT_ERROR_ABORT)
     {
+        unlockInputs();
         return error();
     }
 
+    GEO_Primitive* prim = nullptr;
     fpreal t = context.getTime();
+    UT_Interrupt* boss = UTgetInterrupt();
+
+    UT_Array<GEO_PrimVolume*> volumes;
+
+    GA_FOR_ALL_OPT_GROUP_PRIMITIVES(gdp, m_group, prim)
+    {
+        if(boss->opInterrupt())
+        {
+            break;
+        }
+
+        if(prim->getTypeId() == GEO_PRIMVOLUME)
+        {
+            GEO_PrimVolume* volume = (GEO_PrimVolume *) prim;
+            volumes.append(volume);
+        }
+    }
+
+    if(volumes.entries() > 0)
+    {
+        processVolumes(volumes, t);
+    }
+    else
+    {
+        UT_WorkBuffer buf;
+        buf.sprintf("No input volumes found.");
+        addError(SOP_MESSAGE, buf.buffer());
+    }
+
+    unlockInputs();
 
     return error();
 }
@@ -61,6 +95,20 @@ const char*
 SOP_VoxelCenterPoints::inputLabel(unsigned int idx) const
 {
     return "Voxels to Center Points";
+}
+
+
+OP_ERROR
+SOP_VoxelCenterPoints::cookInputGroups(OP_Context& context, int alone)
+{
+    return cookInputPrimitiveGroups(context, m_group, m_detail_group_pair, alone);
+}
+
+
+void
+SOP_VoxelCenterPoints::processVolumes(const UT_Array<GEO_PrimVolume*>& volumes, fpreal t)
+{
+
 }
 
 
--- SOP_VoxelCenterPoints.h
+++ SOP_VoxelCenterPoints.h
@@ -3,13 +3,18 @@
 #include <SOP/SOP_API.h>
 #include <SOP/SOP_Node.h>
 
+class GEO_PrimVolume;
+
 class SOP_API SOP_VoxelCenterPoints : public SOP_Node
 {
     public:
 
         static OP_Node* myConstructor(OP_Network* network, const char* name, OP_Operator* op);
-
         static PRM_Template myTemplateList[];
+
+    public:
+
+        virtual OP_ERROR cookInputGroups(OP_Context& context, int alone = 0);
 
     protected:
 
@@ -20,4 +25,13 @@ class SOP_API SOP_VoxelCenterPoints : public SOP_Node
 
         virtual const char* inputLabel(unsigned int idx) const;
         virtual OP_ERROR cookMySop(OP_Context& context);
+
+    protected:
+
+        void processVolumes(const UT_Array<GEO_PrimVolume*>& volumes, fpreal t);
+
+    private:
+
+        GU_DetailGroupPair m_detail_group_pair;
+        const GA_PrimitiveGroup* m_group;
 };
